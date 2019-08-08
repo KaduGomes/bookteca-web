0bbff37bd8395fa8ae943b58eda79f236fa13370
Carlos Eduardo Gomes Almeida <kadugomes343@gmail.com>
2019-08-07 22:31:04 -0300

    Criação de validaçâo de formulário com JQuery Validation

--- livro.js
+++ livro.js
@@ -1,22 +1,54 @@
 window.onload = () => {
   let elementoBotao = document.querySelector('#cadastrar')
 
-  elementoBotao.addEventListener('click', (event) =>{
-    event.preventDefault()
-    console.log('Clicou no botâo');
-    let ladda = Ladda.create(elementoBotao)
-    ladda.start()
-    ladda.setProgress(0.2)
-    let pregressoAtual = 0
-    let interval =
-    setInterval(() => {
-      ladda.setProgress(pregressoAtual += 0.1)
-      if (pregressoAtual>= 1){
-        clearInterval(interval)
-        ladda.stop()
+  // elementoBotao.addEventListener('click', (event) =>{
+  //   event.preventDefault()
+  //   console.log('Clicou no botâo');
+  //   let ladda = Ladda.create(elementoBotao)
+  //   ladda.start()
+  //   ladda.setProgress(0.2)
+  //   let pregressoAtual = 0
+  //   let interval =
+  //   setInterval(() => {
+  //     ladda.setProgress(pregressoAtual += 0.1)
+  //     if (pregressoAtual>= 1){
+  //       clearInterval(interval)
+  //       ladda.stop()
+  //     }
+  //   }, 200)
+  // })
+  let formularioLivro = $('#formLivro')
+  formularioLivro.validate({
+    rules: {
+      titulo: {
+        required: true,
+        minlength: 3
+      },
+      subtitulo: {
+        required: false,
+        minlength: 9
+      },
+      descricao: {
+        required: true,
+        minlength: 6
       }
-
-    }, 200)
+    },
+    messages: {
+      titulo: {
+        required: 'O titulo é obrigatório',
+        minlength: 'O titulo é muito curto'
+      },
+      subtitulo:{
+        minlength: 'O subtitulo é muito curto'
+      },
+      descricao:{
+        required: 'A descrição é obrigatório',
+        minlength:'A descrição esta curta demais'
+      }
+    },
+    errorPlacement: function(error, element){
+        element.parent().parent().find('.error').append(error).addClass('animated flash')
+    },
+    errorClass: 'cor-do-erro'
   })
-
 }
